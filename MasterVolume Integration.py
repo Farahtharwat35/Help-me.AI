0e80dbdf08dbf7eb9669844da7fcdf7aa38e1815
Farahtharwat35 <farahtharwat35@gmail.com>
2023-04-15 01:28:39 +0200

    Volume done :  to change volume make High-Five -> increase volume  : two fingers  decrease volume: 3 fingers

--- MasterVolume Integration.py
+++ MasterVolume Integration.py
@@ -204,39 +204,40 @@ while True:
                 # This gives the pixel coordinates of the landmark point in the image.
                 lmList.append([id, cx, cy])
             mpDraw.draw_landmarks(img, handlandmark, mpHands.HAND_CONNECTIONS)  #draw all the landmarks
-    one=1
+
     two=2
+    three=3
 
     if lmList != []:
 
         ##Set MasterVolume
         if (volumeFlag):
             print ("FLAGG")
-            ##Points for thumb and index
-            x1, y1 = lmList[4][1], lmList[4][2]
-            x2, y2 = lmList[8][1], lmList[8][2]
+            # ##Points for thumb and index
+            # x1, y1 = lmList[4][1], lmList[4][2]
+            # x2, y2 = lmList[8][1], lmList[8][2]
+            #
+            # ##Draw circle on tips
+            # cv2.circle(img, (x1, y1), 15, (255, 0, 0), cv2.FILLED)
+            # cv2.circle(img, (x2, y2), 15, (255, 0, 0), cv2.FILLED)
 
-            ##Draw circle on tips
-            cv2.circle(img, (x1, y1), 15, (255, 0, 0), cv2.FILLED)
-            cv2.circle(img, (x2, y2), 15, (255, 0, 0), cv2.FILLED)
-
-            ##Draw a line in between
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
-
-            ##Getting length between fingers
-            length = hypot(x2 - x1, y2 - y1)
+            # ##Draw a line in between
+            # cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+            #
+            # ##Getting length between fingers
+            # length = hypot(x2 - x1, y2 - y1)
 
             # for i in count.values() :
             #     print(i)
 
-            if one in count.values() or two in count.values():
+            if two in count.values() or three in count.values():
                 ##converting hand range to volume range
                 #vol = np.interp(length, [60, 260], [volMin, volMax])
                 # #print(vol, length)  # for debugging
                 currentVolume=volume.GetMasterVolumeLevel()
                 print("currentVolume ",currentVolume)
 
-                if (one in count.values()):
+                if (two in count.values()):
                     if (currentVolume  >= volMin and currentVolume < volMax):
                         print("da5alt")
                         if (currentVolume >=volMin and currentVolume <= -24.0  ):
@@ -307,11 +308,11 @@ while True:
                 ThumbY = lms[2]
         if ThumbY == indexY:
             flag = True
-        # if (middleY < handBottomY) and (ringY < handBottomY) and (pinkyY < handBottomY) and (middleY > middleBottomY) and (ringY > ringBottomY) and (pinkyY>pinkyBottomY):
-        #     #cv2.rectangle(handsFrame, (indexX, indexY), (pinkyX, handBottomY), (0, 0, 255), 2)
-        #     #cv2.putText(handsFrame, fistWarning, (pinkyX + 2, indexY - 2), (font), .7,(0, 0, 255), 1, cv2.LINE_4)
-        #     #print("Fist!!")
-        #     volumeFlag = False
+        if (middleY < handBottomY) and (ringY < handBottomY) and (pinkyY < handBottomY) and (middleY > middleBottomY) and (ringY > ringBottomY) and (pinkyY>pinkyBottomY):
+            #cv2.rectangle(handsFrame, (indexX, indexY), (pinkyX, handBottomY), (0, 0, 255), 2)
+            #cv2.putText(handsFrame, fistWarning, (pinkyX + 2, indexY - 2), (font), .7,(0, 0, 255), 1, cv2.LINE_4)
+            print("Fist!!")
+            volumeFlag = False
 
     cv2.imshow('Image', img)
 
