7d2114a3eeb22a6cf7ade6ff2bf4a5e91ca634de
rafikghaly <40712708+rafikghaly@users.noreply.github.com>
2023-04-26 20:24:23 +0200

    Added reading gestures from DB

--- Integrator.py
+++ Integrator.py
@@ -1,7 +1,15 @@
 # This file is for Integrating all functions into the system
-import screenShot
 
-
-def gestureChooser(gest):
-    if gest == 'HIGH-FIVE SIGN':
-        screenShot.take_screenshot()
+def gestureChooser(gesture):
+    with open('gestures.txt', 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            columns = line.split('/')
+            txt_gesture = columns[0]
+            txt_module = columns[1]
+            txt_function = columns[2]
+            # print(gesture, " ", action, " ", function)
+            if gesture == txt_gesture:
+                module = __import__(txt_module)
+                func = getattr(module, txt_function)
+                func()
