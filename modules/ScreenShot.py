b3d3cf0be8a1ec337ce25e9003ee694b95d03a06
silentunicorn249 <markbassem@hotmail.com>
2023-07-02 18:00:03 +0300

    reformatted project structure

--- modules/ScreenShot.py
+++ modules/ScreenShot.py
@@ -3,5 +3,5 @@ from PIL import ImageGrab
 from datetime import datetime
 def take_screenshot():
     screenshot = ImageGrab.grab()
-    readable_time = datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d0_%H_%M_%S')
+    readable_time = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H_%M_%S')
     screenshot.save(f"./Screenshots/{readable_time}.jpg")
